914da2055355be22ec7ac4aeff762c50ad6cb679
Destiny Rose <destinyrose2358@gmail.com>
2019-08-21 19:40:48 -0500

    Corrects 03 queries in movie buff exercise

--- movie_buff/movie_buff/03_queries.rb
+++ movie_buff/movie_buff/03_queries.rb
@@ -1,11 +1,10 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
-  Movie
-    .select(:id, :title)
-    .joins(:actors)
-    .where(actors: { name: those_actors})
-    .where(castings: { ord: 1})
+  movies  = Movie.all.includes(:actors)
+  movies.select do |movie|
+    movie.actors.all? { }
+  end
 end
 
 def golden_age
